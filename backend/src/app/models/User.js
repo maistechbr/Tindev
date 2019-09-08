8b4e328265339adb7821c50fdb8c74963bf18276
Daniel Felizardo <daniel.david772@gmail.com>
2019-09-07 23:16:29 -0300

    chore: Adding migration for user

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,13 +1,25 @@
-import { Model } from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
+import bcrycpt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
     super.init(
-      {},
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrycpt.hash(user.password, 8);
+      }
+    });
 
     return this;
   }
