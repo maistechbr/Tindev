9972e03eb21d0c382fb1a204d3edc065d668bd72
Daniel Felizardo <daniel.david772@gmail.com>
2019-09-24 00:44:30 -0300

    config: rate-limit-redis and cors

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -6,6 +6,9 @@ import cors from 'cors';
 import helmet from 'helmet';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 
 import 'express-async-errors';
 import sentryConfig from './config/sentry';
@@ -28,8 +31,27 @@ class App {
   middleware() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(helmet());
-    this.server.use(cors());
+    this.server.use(
+      cors({
+        origin: process.env.FRONT_URL || false,
+      })
+    );
     this.server.use(express.json());
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
